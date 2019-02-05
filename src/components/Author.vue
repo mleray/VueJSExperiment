a86aacb5f78e67e419f3d50aa71c10963937f4bb
mleray <maudleray035@gmail.com>
2019-02-06 00:05:03 +0100

    Create Author component

---
+++ src/components/Author.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class='author'>
+        <img
+            :src='imageUrl'
+            height='60'
+            width='60'
+        />
+        <p>{{ name }}</p>
+    </div>
+</template>
+
+<script lang='ts'>
+import Vue from 'vue';
+
+export default Vue.extend({
+    props: {
+        name: String,
+        imageUrl: String
+    }
+});
+</script>
+
+<style>
+.author {
+    text-align: center;
+    margin-right: 10px;
+}
+.author p {
+    margin-top: 0;
+}
+img {
+    border-radius: 50%;
+}
+</style>
