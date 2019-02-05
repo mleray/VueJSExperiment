a6982814bc99d5b7520463386eb5dce1c4cf7234
mleray <maudleray035@gmail.com>
2019-02-06 00:05:14 +0100

    Update BlogPost component

--- src/components/BlogPost.vue
+++ src/components/BlogPost.vue
@@ -1,23 +1,49 @@
 <template>
-    <div class="blog-post" :style="{ borderColor: color }">
-        <p class="title">{{ title }}</p>
-        <p class="author">{{ author }}</p>
-        <p class="content">{{ content }}</p>
-        <p class="date">{{ date }}</p>
+    <div class='blog-post' :style='{ borderColor: color }'>
+        <p class='date'>{{ publishDate }}</p>
+        <a v-on:click='views += 1' :href='blogPostUrl' target='_blank'>
+            <h3 class='title' :style='{ color }'>{{ title }}</h3>
+        </a>
+        <div class='flex'>
+            <author
+                v-for='(author, index) in authors'
+                :key='index'
+                :name='author.name'
+                :imageUrl='author.image.url'
+            />
+        </div>
+        <p class='views'>{{ viewsCounter }}</p>
     </div>
 </template>
 
-<script lang="ts">
-import Vue from "vue";
+<script lang='ts'>
+import Vue from 'vue';
+import Author from './Author.vue';
 
 export default Vue.extend({
-    props: [
-        'author',
-        'color',
-        'content',
-        'date',
-        'title',
-    ]
+    props: {
+        title: String,
+        color: String,
+        publishDate: String,
+        authors: Array,
+        slug: String
+    },
+    data() {
+        return {
+            views: 0
+        }
+    },
+    components: {
+        Author
+    },
+    computed: {
+        blogPostUrl() {
+            return 'https://www.voorhoede.nl/en/blog/' + this.slug;
+        },
+        viewsCounter() {
+            return 'Views: ' + this.views;
+        }
+    }
 });
 </script>
 
@@ -25,8 +51,25 @@ export default Vue.extend({
 .blog-post {
     border-style: solid;
     border-width: medium;
-    padding: 20px;
-    margin-top: 20px;
+    padding: 10px 20px;
+    margin: 20px;
     width: 300px;
+    flex: auto;
+    position: relative;
+}
+.date {
+    float: right;
+    font-size: 12px;
+    margin-top: 0;
+}
+.views {
+    font-size: 14px;
+    font-weight: bold;
+    right: 20px;
+    bottom: 0;
+    position: absolute;
+}
+a {
+    text-decoration: none;
 }
 </style>
